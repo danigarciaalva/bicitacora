9eaf25826b028d20d80ac4bbfebb533619b60045
SrAlex <alex_machine2@hotmail.com>
2013-10-27 13:10:27 -0600

    cambio en diseños

--- Bicitacora/gen/com/example/android/effectivenavigation/R.java
+++ Bicitacora/gen/com/example/android/effectivenavigation/R.java
@@ -20,21 +20,17 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f050001;
-        public static final int button1=0x7f05000f;
+        public static final int button1=0x7f05000a;
         public static final int correo=0x7f050004;
-        public static final int editText1=0x7f050009;
-        public static final int editText2=0x7f05000a;
-        public static final int editText3=0x7f05000c;
         public static final int entrar=0x7f050006;
+        public static final int nombre=0x7f050008;
         public static final int pager=0x7f050000;
         public static final int password=0x7f050005;
+        public static final int password2=0x7f05000b;
         public static final int pause=0x7f050002;
         public static final int registro=0x7f050007;
         public static final int start_stop=0x7f050003;
-        public static final int textView1=0x7f050008;
-        public static final int textView2=0x7f05000b;
-        public static final int textView3=0x7f05000d;
-        public static final int textView4=0x7f05000e;
+        public static final int textView4=0x7f050009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
